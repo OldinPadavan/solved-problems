5825ced890905b2e608c7358b9395c5371776c36
workstation1 <diyandmakers@gmail.com>
2023-01-27 22:07:20 +0300

    finished memFromBoys method
    added demo version for memFromBoys method, tests did

--- src/main/java/CodeWars/SortMemBoy.java
+++ src/main/java/CodeWars/SortMemBoy.java
@@ -8,6 +8,7 @@ import java.util.stream.IntStream;
 public class SortMemBoy {
     public static void main(String[] args) {
 
+        System.out.println(menFromBoys(new int[] {7, 3, 14, 17}).toString() + "....." + new int[] {14, 17, 7, 3}.toString());
     }
 
     public static int[] menFromBoys(final int[] values) {
@@ -15,6 +16,10 @@ public class SortMemBoy {
         Arrays.sort(oddNumbers);
         int[] evenNumbers = Arrays.stream(values).filter(x-> x % 2 == 0).toArray();
         Arrays.sort(evenNumbers);
-        return null;// mock
+        int[] evenNumbersDescSort = new int[evenNumbers.length];
+        for (int i = evenNumbersDescSort.length-1, y = 0; i >= 0; i--, y++) {
+            evenNumbersDescSort[y] = evenNumbersDescSort[i];
+        }
+        return IntStream.concat(Arrays.stream(oddNumbers), Arrays.stream(evenNumbersDescSort)).toArray();
     }
 }
