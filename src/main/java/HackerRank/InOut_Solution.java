20662070db8e29303b2f08b44f3ed9a31b4670e3
OldinPadawan <diyandmakers@gmail.com>
2023-01-31 11:38:45 +0300

    added solution for InOut problem from HackerRank, added class for PaperFold task from CodeWars

--- src/main/java/HackerRank/InOut_Solution.java
+++ src/main/java/HackerRank/InOut_Solution.java
@@ -1,18 +1,24 @@
 package HackerRank;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class InOut_Solution {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int i = scan.nextInt();
-        double d = scan.nextDouble();
-        String s = scan.next();
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            int i = Integer.parseInt(reader.readLine());
+            double d = Double.parseDouble(reader.readLine());
+            String s = reader.readLine();
 
-        // Write your code here.
+            System.out.println(i);
+            System.out.println(d);
+            System.out.println(s);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
 
-        System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
     }
+
 }
