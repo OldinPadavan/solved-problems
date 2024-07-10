00b33b2f88b6b583db59f080784b90e05f7fca96
workstation1 <diyandmakers@gmail.com>
2024-07-10 10:35:50 +0300

    created scanner method for LoopTwo

--- src/main/java/HackerRank/LoopTwo_Solution.java
+++ src/main/java/HackerRank/LoopTwo_Solution.java
@@ -1,6 +1,38 @@
 package HackerRank;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 //Basic information about task taken from here
 // https://www.hackerrank.com/challenges/java-loops/problem?isFullScreen=true
 public class LoopTwo_Solution {
+    public static void main(String[] args) {
+        int q = numScanner();
+        Integer[] numbers = null;
+        if (q >=0 & q <= 500) {
+            for (int i = 0; i < q; i++) {
+                 numbers = num_seq_Scanner().toArray(new Integer[0]);
+            }
+            for (Integer integer : numbers) {
+                System.out.print(integer + " ");
+            }
+
+        }
+    }
+
+    public static List<Integer> num_seq_Scanner() {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer>inputNumbers = new ArrayList<>();
+        while (scanner.hasNextInt()) {
+            inputNumbers.add(scanner.nextInt());
+        }
+        scanner.close();
+        return inputNumbers;
+    }
+
+    public static int numScanner() {
+        return new Scanner(System.in).nextInt();
+    }
 }
